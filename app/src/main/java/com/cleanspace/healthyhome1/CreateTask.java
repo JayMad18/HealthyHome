1beee4199d2e19c367260b8178fd4bfb88ee88c8
JayMad18 <lordxm.madison@gmail.com>
2021-09-06 04:33:23 -0400

    I havent worked on this app in a while, It is long over-due. This commit is one that was supposed to be made long ago, As far as I can tell this commit adds an ArrayAdapter and a ListView to the CreateTask.java class and the activity_create_task.xml class. I believe I was going to display all memebers of the Home in this listview so that the user could click on a member and assign that member to that task. May be changed in the future as I will try to finish this app as soon as possible so that I can start a new app project.

--- app/src/main/java/com/cleanspace/healthyhome1/CreateTask.java
+++ app/src/main/java/com/cleanspace/healthyhome1/CreateTask.java
@@ -3,22 +3,67 @@ package com.cleanspace.healthyhome1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateTask extends AppCompatActivity {
     EditText taskNameEditText, userNameEditText;
-    TextView taskNameTextView, assignToTextView;
+    ArrayList<ParseUser> membersOfHome = new ArrayList<ParseUser>();
+    ListView membersListView;
+    String selectedHomeObjectId;
+    ArrayList<ParseUser> parseUsers = new ArrayList<ParseUser>();
+    ArrayList<String> memberNames = new ArrayList<String>();
+    ArrayList<String> memberObjectIds = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
         taskNameEditText = findViewById(R.id.taskNameEditText);
-        assignToTextView = findViewById(R.id.assignToTextView);
+        userNameEditText = findViewById(R.id.userNameEditText);
+        membersListView = findViewById(R.id.membersListView);
+        populateListView();
     }
     public void createTask(View view){
-        //New task parse object here
+        ParseObject task = new ParseObject("Tasks");
+        task.put("Name", taskNameEditText.getText().toString());
+        ParseQuery<ParseUser> loadUsers = ParseUser.getQuery();
+    }
+    public void populateListView(){
+        ArrayAdapter<String> memberNamesAdapter = new ArrayAdapter<String>(this, R.layout.list_layout, R.id.list_content,memberNames);
+
+        ParseQuery<ParseUser> memberQuery = ParseUser.getQuery();
+        memberQuery.whereContains("HomeList", selectedHomeObjectId);
+        memberQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e == null){
+                    for(ParseUser user: objects){
+                        memberNames.add(user.get("name").toString());
+                        memberObjectIds.add(user.getObjectId());
+                        parseUsers.add(user);
+                    }
+                    memberNamesAdapter.notifyDataSetChanged();
+                    membersListView.setAdapter(memberNamesAdapter);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Error loading members "+e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                    Log.d("error loading homes",e.getLocalizedMessage());
+                }
+            }
+        });
     }
 }
