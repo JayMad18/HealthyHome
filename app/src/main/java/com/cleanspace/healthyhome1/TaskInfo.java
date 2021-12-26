b5b6430ed16f2ab0f3c9e87728e1ba64bcab8487
JayMad18 <lordxm.madison@gmail.com>
2021-12-25 22:14:34 -0500

    Push Notification when task completed added a notification to all members of the home when a member marks a task complete.

--- app/src/main/java/com/cleanspace/healthyhome1/TaskInfo.java
+++ app/src/main/java/com/cleanspace/healthyhome1/TaskInfo.java
@@ -13,6 +13,10 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.DeleteCallback;
 import com.parse.FindCallback;
@@ -22,8 +26,13 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TaskInfo extends AppCompatActivity {
 
@@ -33,6 +42,7 @@ public class TaskInfo extends AppCompatActivity {
 
     ParseObject assignedToObject;
     String taskObjectId;
+    String TOPIC;
     ParseUser completedByUserObject;
 
     ParseObject task;
@@ -47,6 +57,7 @@ public class TaskInfo extends AppCompatActivity {
         setContentView(R.layout.activity_task_info);
 
         sentTaskInfo = getIntent();
+        TOPIC = "/topics/"+sentTaskInfo.getStringExtra("HomeObjectID") + "TOPIC";
 
         setBottomNavListener();
 
@@ -166,23 +177,54 @@ public class TaskInfo extends AppCompatActivity {
         });
     }
     public void completeTask(View button){
-        new AlertDialog.Builder(this).setTitle("Complete Task").setMessage("Are you sure that this task is complete?")
-                .setPositiveButton("Yea", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                       complete();
-                    }
-                }).setNegativeButton("Not yet..", null).show();
+        if(isAssigned){
+            if(ParseUser.getCurrentUser().getObjectId().equals(assignedToObject.getObjectId())){
+                new AlertDialog.Builder(this).setTitle("Complete Task").setMessage("Are you sure that this task is complete?")
+                        .setPositiveButton("Yea", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                complete();
+                            }
+                        }).setNegativeButton("Not yet..", null).show();
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Only the member assigned to this task may mark task completed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            new AlertDialog.Builder(this).setTitle("Complete Task").setMessage("Are you sure that this task is complete?")
+                    .setPositiveButton("Yea", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            complete();
+                        }
+                    }).setNegativeButton("Not yet..", null).show();
+        }
+
     }
 
     public void deleteTask(View button){
-        new AlertDialog.Builder(this).setTitle("Delete Task").setMessage("Are you sure you want to delete this task? ")
-                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        delete();
-                    }
-                }).setNegativeButton("Cancel", null).show();
+        String assignerObjectId = task.get("assignerObjectId").toString();
+        if(assignerObjectId.equals(ParseUser.getCurrentUser().getObjectId())){
+            new AlertDialog.Builder(this).setTitle("Delete Task").setMessage("Are you sure you want to delete this task? ")
+                    .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if(assignerObjectId.equals(ParseUser.getCurrentUser().getObjectId())){
+                                delete();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),"Only the member who assigned the task may delete the task",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }).setNegativeButton("Cancel", null).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Only the member who assigned the task may delete the task",Toast.LENGTH_SHORT).show();
+        }
+
     }
     public void complete(){
         if(isAssigned){
@@ -195,10 +237,8 @@ public class TaskInfo extends AppCompatActivity {
                         if(e == null){
                             statusTextView.setText("Completed");
                             statusTextView.setTextColor(getColor(R.color.DeepPink));
-
                             completedByTextView.setText("Completed by "+assignedToObject.get("name").toString());
-                            //TODO: send notification to all members of home that task has been completed
-                        }
+                            buildJSONMessageObject();                        }
                         else{
                             Toast.makeText(getApplicationContext(),"Failed marking task complete "+e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
 
@@ -222,7 +262,7 @@ public class TaskInfo extends AppCompatActivity {
                         statusTextView.setText("Completed");
                         completedByTextView.setText("Completed by "+ParseUser.getCurrentUser().get("name").toString());
                         statusTextView.setTextColor(getColor(R.color.DeepPink));
-                        //TODO: send notification to all members of home that task has been completed
+                        buildJSONMessageObject();
                     }
                     else{
                         Toast.makeText(getApplicationContext(),"Failed marking task complete "+e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
@@ -247,6 +287,69 @@ public class TaskInfo extends AppCompatActivity {
             }
         });
     }
+
+    /*
+     *create a JsonObject of the notification body
+     * This object will contain the receiver’s topic, notification title,
+     * notification message, and other key/value pairs to add.
+     * */
+    public void buildJSONMessageObject(){
+        JSONObject notification = new JSONObject();
+        JSONObject notificationBody = new JSONObject();
+        if(isAssigned == true){
+            try {
+                notificationBody.put("title", "Task completed!");
+                notificationBody.put("message", ParseUser.getCurrentUser().getUsername()+" has marked " +
+                        task.get("Name").toString() +" completed!");
+
+                notification.put("to", TOPIC);
+                //notification.put("registration_ids",registrationTokens);
+                notification.put("data", notificationBody);
+            } catch ( JSONException e) {
+            }
+        }
+        else{
+            try {
+                notificationBody.put("title", "New Task");
+                notificationBody.put("message", ParseUser.getCurrentUser().getUsername()+" has created a new task: " + task.get("Name"));
+
+                notification.put("to", TOPIC);
+                //notification.put("registration_ids",registrationTokens);
+                notification.put("data", notificationBody);
+            } catch ( JSONException e) {
+            }
+        }
+        sendNotification(notification);
+    }
+
+    /*
+     *make a network request to FCM server using Volley library,
+     *then the server will use the request parameters to route the
+     *notification to the targeted device.
+     * */
+    public void sendNotification(JSONObject notification){
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(getString(R.string.FCM_API), notification,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Authorization","key="+getString(R.string.server_key));
+                params.put("Content-Type", getString(R.string.content_type));
+                return params;
+            }
+        };
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+
     //helper method to quickly switch activites
     public void changeActivity(Class activity){
         Intent switchActivity = new Intent(getApplicationContext(), activity);
